af40528b963cd2b7c2202f2ef68da7e59393f4eb
su-mio <yoshi.koshien@gmail.com>
2015-05-29 20:46:55 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Himawari::Application.config.secret_key_base = '86c00eac1fed3fcb8be8819821293fe9893b55529dfa511a99f8fee9a1093566b0a649746de0a3fab3e6c5c676e1937d25a88a81a39e54b09fae79b612ec5fbd'
